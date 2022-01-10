5704115a9602a57fed80c9c2d41b4e7ba51396f3
Holger Wegert <holger.wegert@pqp.systems>
2022-01-10 23:50:04 +0100

    Vergleich von Titeln verbessert

--- src/main/java/systems/pqp/hsdb/RadioPlayType.java
+++ src/main/java/systems/pqp/hsdb/RadioPlayType.java
@@ -2,14 +2,17 @@ package systems.pqp.hsdb;
 
 import de.ard.sad.normdb.similarity.compare.basic.date.FuzzyDateSimilarity;
 import de.ard.sad.normdb.similarity.compare.basic.string.EqualSimilarity;
+import de.ard.sad.normdb.similarity.compare.basic.string.FuzzyStringVariantSimilarity;
+import de.ard.sad.normdb.similarity.compare.basic.string.ScaledLevensteinWithPartTest;
+import de.ard.sad.normdb.similarity.compare.ndb.name.InstitutionNameSimilarity;
 import de.ard.sad.normdb.similarity.compare.ndb.name.PersonNameSimilarity;
 import de.ard.sad.normdb.similarity.model.generic.GenricObjectType;
 import de.ard.sad.normdb.similarity.model.generic.types.BasicType;
 
 public class RadioPlayType extends BasicType {
-    public static final GenricObjectType TITLE = new GenricObjectType("string.title", 0.9f, 0.0f, true, true, true, new PersonNameSimilarity());
-    public static final GenricObjectType SHOW_TITLE = new GenricObjectType("string.title.show", 0.9f, 0.0f, false, false, false, new PersonNameSimilarity());
-    public static final GenricObjectType LONG_TITLE = new GenricObjectType("string.title.long", 1.0f, 0.0f, false, false, false, new EqualSimilarity());
+    public static final GenricObjectType TITLE = new GenricObjectType("string.title", 0.9f, 0.0f, true, true, true, new FuzzyStringVariantSimilarity(new InstitutionNameSimilarity(new ScaledLevensteinWithPartTest(), 2.0d, 0.5d, true, 0.0f), FuzzyStringVariantSimilarity.OutputSetting.MAX));
+    public static final GenricObjectType SHOW_TITLE = new GenricObjectType("string.title.show", 0.9f, 0.0f, false, false, false, new FuzzyStringVariantSimilarity(new InstitutionNameSimilarity(new ScaledLevensteinWithPartTest(), 2.0d, 0.5d, true, 0.0f), FuzzyStringVariantSimilarity.OutputSetting.MAX));
+    public static final GenricObjectType LONG_TITLE = new GenricObjectType("string.title.long", 1.0f, 0.0f, false, false, false, new FuzzyStringVariantSimilarity(new InstitutionNameSimilarity(new ScaledLevensteinWithPartTest(), 2.0d, 0.5d, true, 0.0f), FuzzyStringVariantSimilarity.OutputSetting.MAX));
     public static final GenricObjectType BIO = new GenricObjectType("string.bio", 1.0f, 0.0f, false, false, false, new EqualSimilarity());
     public static final GenricObjectType DESCRIPTION = new GenricObjectType("string.description", 1.0f, 0.0f, false, false, false, new EqualSimilarity());
     public static final GenricObjectType DURATION = new GenricObjectType("float.duration", 0.9f, 0.89999f, false, true, true, new FloatPercentageSimilarity());
