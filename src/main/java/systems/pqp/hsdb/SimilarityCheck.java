70a223b0ad8dcd825edca2f6384b2cb3b0956512
Holger Wegert <holger.wegert@pqp.systems>
2022-02-17 23:24:37 +0100

    Logausgaben um Durchschnittswert angepasst

--- src/main/java/systems/pqp/hsdb/SimilarityCheck.java
+++ src/main/java/systems/pqp/hsdb/SimilarityCheck.java
@@ -1,7 +1,6 @@
 package systems.pqp.hsdb;
 
 import de.ard.sad.normdb.similarity.model.generic.GenericObject;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import systems.pqp.hsdb.dao.HsdbDao;
@@ -21,27 +20,36 @@ public class SimilarityCheck {
     List<SimilarityBean> foundSimilarities = null;
     Map<String, Integer> state = Collections.synchronizedMap(new HashMap<>());
     int total = 0;
+    long startTime = System.currentTimeMillis();
 
-    synchronized void updateState(String key){
-        if(state.containsKey(key)){
-            state.replace(key,state.get(key) + 1);
+    synchronized void updateState(String key) {
+        if (state.containsKey(key)) {
+            state.replace(key, state.get(key) + 1);
         } else {
-            state.put(key,1);
+            state.put(key, 1);
         }
 
         logStatus();
     }
 
-    synchronized void logStatus(){
+    synchronized void logStatus() {
+        float duration = (System.currentTimeMillis() - startTime) / 1000.0f;
+        Integer totalProcessed = state.get("Total");
+
         StringBuilder message = new StringBuilder();
         message.append("\n##### Status #####\n");
         message.append("Total");
         message.append(": ");
-        message.append(state.get("Total"));
+        message.append(totalProcessed);
         message.append("/").append(total);
+        message.append(" seit ");
+        message.append(duration);
+        message.append(" Sekunden bei ⌀ ");
+        message.append((totalProcessed / (duration / 60.0f)));
+        message.append(" pro Minute");
         message.append("\n");
-        for(Map.Entry<String, Integer> s: state.entrySet()){
-            if(!s.getKey().equals("Total")) {
+        for (Map.Entry<String, Integer> s : state.entrySet()) {
+            if (!s.getKey().equals("Total")) {
                 message.append(s.getKey());
                 message.append(": ");
                 message.append(s.getValue());
@@ -52,20 +60,20 @@ public class SimilarityCheck {
     }
 
 
-    public SimilarityCheck(){}
+    public SimilarityCheck() {}
 
     /**
      * Berechnet die Gleichheit zweier GenericObject-Objekte.
-     * @param hsdbObject GenericObject, Objekt aus HSDB-Datenbank
+     *
+     * @param hsdbObject      GenericObject, Objekt aus HSDB-Datenbank
      * @param audiothekObject GenericObject, Objekt aus Audiothek
      * @return boolean, true wenn gleich
      */
-    public boolean checkSimilarity(RadioPlaytypeSimilarity similarityTest, GenericObject hsdbObject, GenericObject audiothekObject){
+    public boolean checkSimilarity(RadioPlaytypeSimilarity similarityTest, GenericObject hsdbObject, GenericObject audiothekObject) {
         return Float.parseFloat(config.getProperty(Config.THRESHOLD)) <= similarityTest.calcSimilarity(hsdbObject, audiothekObject);
     }
 
     /**
-     *
      * @param hsdbObjects
      * @param audiothekObjects
      * @param numThreads
@@ -79,11 +87,11 @@ public class SimilarityCheck {
 
         LOG.info("Starte Vergleich für {} Audiothek-Objekte", audiothekObjects.size());
         total = audiothekObjects.size();
-        IntegerBucketingCache cache = new IntegerBucketingCache(hsdbObjects,RadioPlayType.DURATION);
+        IntegerBucketingCache cache = new IntegerBucketingCache(hsdbObjects, RadioPlayType.DURATION);
 
         ForkJoinPool threads = new ForkJoinPool(numThreads);
         LOG.info("Initialisierte Threads: {}", threads.getParallelism());
-
+        startTime = System.currentTimeMillis();
         try {
             threads.submit(
                     () -> audiothekObjects.values().parallelStream().forEach(
@@ -95,7 +103,7 @@ public class SimilarityCheck {
         }
 
         LOG.info("Similarities: {}", foundSimilarities.size());
-        if( !foundSimilarities.isEmpty() ){
+        if (!foundSimilarities.isEmpty()) {
             HsdbDao dao = new HsdbDao();
             dao.upsertMany(foundSimilarities);
         }
@@ -105,12 +113,12 @@ public class SimilarityCheck {
     /**
      *
      */
-    public void map(String audiothekId, GenericObject audiothekObject, IntegerBucketingCache cache){
+    public void map(String audiothekId, GenericObject audiothekObject, IntegerBucketingCache cache) {
         Instant instant = Instant.ofEpochMilli(System.currentTimeMillis());
         LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
         RadioPlaytypeSimilarity similarityTest = new RadioPlaytypeSimilarity();
 
-        List<GenericObject> hsdbBucket = cache.searchByNumeric(audiothekObject.getProperties(RadioPlayType.DURATION).get(0).getDescriptions().get(0),0.12f);
+        List<GenericObject> hsdbBucket = cache.searchByNumeric(audiothekObject.getProperties(RadioPlayType.DURATION).get(0).getDescriptions().get(0), 0.12f);
 
         hsdbBucket.forEach(
                 hsdbGenericObject -> {
